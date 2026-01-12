c752458c57344ba1138465249ad8f0fbee626b8f
zbrown3 <ziare.brown@proton.me>
2026-01-11 18:03:07 -0600

    added comments

--- main.go
+++ main.go
@@ -7,18 +7,23 @@ import (
 
 
 func main() {
+
+	// Locates all NICs
 	devices, err := pcap.FindAllDevs()
 	if err != nil {
 		panic(err)
 	}
-
+	
+	// Assigns first NIC to device variable
 	device := devices[0].Name
-
+	
+	// Opens the interface for packet capture. Sets the max packet size.  Sets promiscuous mode to true.  Sets duration to indefinite.
 	handle, err := pcap.OpenLive(device, 65536, true, pcap.BlockForever)
 	if err != nil {
 		panic(err)
 	}
-
+	
+	// Creates a packet source and iterates over the channel and process each packet.
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
 	for packet := range packetSource.Packets() {
